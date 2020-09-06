8e501655fc11b5744b74fd49d5cbafa9f911c3b7
arafat.habib <arafat.habib@metrobank.com.ph>
2020-09-06 19:07:38 +0800

    api-route second example

--- routes/api.js
+++ routes/api.js
@@ -2,8 +2,35 @@
 const express = require('express')
 const router = express.Router()
 
-/*  This is a sample API route. */
+/*  Second Example. */
+const players = [
+  { firstname: "eli", lastname: "manning", position: "qb", age: 37, team: "nyg" },
+  { firstname: "tom", lastname: "braddy", position: "qb", age: 41, team: "nep" },
+  { firstname: "jj", lastname: "watt", position: "de", age: 41, team: "hou" }
+]
 
+const teams = [
+  { name: "giants", city: "new york", conference: "nfc" },
+  { name: "patriots", city: "new england", conference: "afc" },
+  { name: "giants", city: "houston", conference: "afc" }
+]
+
+router.get('/players', (req, res) => {
+  res.json({
+    confirmation: 'success',
+    data: players,
+  });
+})
+
+router.get('/teams', (req, res) => {
+  res.json({
+    confirmation: 'success',
+    data: teams,
+  });
+})
+
+/*  First Example. */
+/*
 router.get('/:resource', (req, res) => {
   res.json({
     confirmation: 'success',
@@ -20,5 +47,6 @@ router.get('/:resource/:id', (req, res) => {
     query: req.query // from the url query string
   })
 })
-
+*/
 module.exports = router
+
